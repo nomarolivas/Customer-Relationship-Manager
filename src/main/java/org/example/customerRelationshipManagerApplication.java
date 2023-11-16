b5e9c8ac43c8ec04cbd2a198507bd0da98155b0a
elementalkey <41343085+nomarolivas@users.noreply.github.com>
2023-11-16 14:56:11 -0800

    Added main page

--- src/main/java/org/example/customerRelationshipManagerApplication.java
+++ src/main/java/org/example/customerRelationshipManagerApplication.java
@@ -4,9 +4,9 @@ import org.example.service.crmService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.stereotype.Service;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
